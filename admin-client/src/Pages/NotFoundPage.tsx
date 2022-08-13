d323db08e6c7b3873d85123fa4f9bb64f69d928a
Gleb Kiselev <12bny23gleb@gmail.com>
2022-08-13 21:27:50 +0200

    Add link and style for 'not found page'

--- admin-client/src/Pages/NotFoundPage.tsx
+++ admin-client/src/Pages/NotFoundPage.tsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function NotFoundPage() {
 	return (
-		<main>
-			<h1>404</h1>
+		<main className="flex justify-center items-center h-screen">
+
+			<div className="flex flex-col items-center bg-teal-50 p-5 px-14 shadow-sm">
+				<h1 className="text-8xl">404</h1>
+				<span className="text-2xl mb-3">Page not found</span> 
+				<span className="hover:underline">
+					<Link to="/">HOME</Link>
+				</span>
+			</div>
+
 		</main>
 	);
 }
