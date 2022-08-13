d6762547e8a51cc286f4d9aed23c01d409a2f3d5
Gleb Kiselev <12bny23gleb@gmail.com>
2022-08-13 19:54:58 +0200

    Add 404 page

--- admin-client/src/App.tsx
+++ admin-client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import NewWordPage from "./Pages/NewWordPage";
+import NotFoundPage from "./Pages/NotFoundPage";
 
 function App() {
 	return (
@@ -8,6 +9,10 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage/>}/>
 				<Route path="/new" element={<NewWordPage/>}/>
+				<Route
+					path="*"
+					element={<NotFoundPage/>}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ admin-client/src/Pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+	return (
+		<main>
+			<h1>404</h1>
+		</main>
+	);
+}
