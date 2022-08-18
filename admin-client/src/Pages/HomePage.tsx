b0ea8865a396340356996ba13659ce35f2a16c59
Gleb Kiselev <12bny23gleb@gmail.com>
2022-08-18 17:55:03 +0200

    Add button for moving on new word page

--- admin-client/src/Pages/HomePage.tsx
+++ admin-client/src/Pages/HomePage.tsx
@@ -1,7 +1,20 @@
+import { CircleButton } from "../components/CircleButton";
+import { useNavigate } from "react-router-dom";
+
 export default function HomePage() {
+	const navigate = useNavigate();
+
+	const circleButtonHandler = () => {
+		navigate("/new");
+	};
+	
 	return (
 		<main>
 			<h1 className="text-3xl font-bold">HomePage</h1>
+
+			<div className="fixed bottom-5 right-5">
+				<CircleButton icon="+" onClick={() => circleButtonHandler()}/>
+			</div>
 		</main>
 	);
 }
---
+++ admin-client/src/components/CircleButton.tsx
@@ -0,0 +1,15 @@
+interface CircleButtonProps {
+  icon: string,
+  onClick: () => void
+}
+
+export function CircleButton({icon, onClick}: CircleButtonProps) {
+	return (
+		<button 
+			className="bg-red-500 w-[50px] h-[50px] rounded-full text-white text-2xl" 
+			onClick={() => onClick()}
+		> 
+			{icon}
+		</button>
+	);
+}
