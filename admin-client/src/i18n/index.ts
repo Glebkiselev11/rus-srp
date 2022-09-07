6f035a9da813df8a315a22daf6a063d58802b409
Gleb Kiselev <12bny23gleb@gmail.com>
2022-09-07 21:05:36 +0200

    Frontend: Add srp translation

--- admin-client/src/i18n/index.ts
+++ admin-client/src/i18n/index.ts
@@ -2,13 +2,15 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./en.json";
 import ru from "./ru.json";
+import srp from "./srp.json";
 
 export default i18n
 	.use(initReactI18next) 
 	.init({
 		resources: {
 			en,
-			ru
+			ru,
+			srp
 		},
 		lng: localStorage.getItem("languageKey") || "en",
 		fallbackLng: "en",
@@ -24,5 +26,6 @@ interface ILanguageList {
 
 export const LanguageList: ILanguageList[] = [
 	{ lng: "en", name: "English" }, 
-	{ lng: "ru", name: "Русский" }
+	{ lng: "ru", name: "Русский" },
+	{ lng: "srp", name: "Srpski" }
 ];
