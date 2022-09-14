8decd9458f12b42bd7d291af9c0fcc34c57438b2
Gleb Kiselev <12bny23gleb@gmail.com>
2022-09-14 20:49:09 +0200

    Frontend: Add shadow for add button on hover

--- admin-client/src/components/CircleButton.tsx
+++ admin-client/src/components/CircleButton.tsx
@@ -6,10 +6,10 @@ interface CircleButtonProps {
 export function CircleButton({ icon, onClick }: CircleButtonProps) {
 	return (
 		<button 
-			className="bg-red-500 w-[50px] h-[50px] rounded-full text-white text-2xl" 
+			className="bg-red-500 w-[50px] h-[50px] rounded-full  hover:shadow-xl" 
 			onClick={() => onClick()}
 		> 
-			{icon}
+			<div className="text-white text-4xl  pt-1 h-full">{icon}</div>
 		</button>
 	);
 }
