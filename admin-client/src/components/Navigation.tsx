9cdf9a4b1c11a06592b898f48df2f9f7781ac9c6
Gleb Kiselev <12bny23gleb@gmail.com>
2022-09-16 21:38:46 +0200

    Frontend: Fix z-index; remove extra div

--- admin-client/src/components/Navigation.tsx
+++ admin-client/src/components/Navigation.tsx
@@ -6,12 +6,10 @@ export function Navigation() {
 	const { t } = useTranslation();
 
 	return (
-		<nav className="sticky top-0 flex justify-between items-center px-5 h-[50px] bg-gray-300 shadow-sm">
+		<nav className="sticky top-0 flex justify-between items-center px-5 h-[50px] bg-gray-300 shadow-sm z-10">
 			<Link to="/" className="text-xl">{t("admin-panel")}</Link>
 
-			<div className="flex">
-				<LanguageChanger/>
-			</div>
+			<LanguageChanger/>
 		</nav>
 	);
 }
