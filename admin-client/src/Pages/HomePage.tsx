2bea701c42c5e573574ba1371bcb5102b65c7c9e
Gleb Kiselev <12bny23gleb@gmail.com>
2022-09-16 21:19:38 +0200

    Frontend: Add search input and translation for label of this input

--- admin-client/src/Pages/HomePage.tsx
+++ admin-client/src/Pages/HomePage.tsx
@@ -2,9 +2,14 @@ import { CircleButton } from "../components/CircleButton";
 import { useNavigate } from "react-router-dom";
 import { useGetAllQuery } from "../store/words";
 import { WordItem } from "../components/WordItem";
+import { TextInput } from "../components/TextInput";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 export default function HomePage() {
+	const { t } = useTranslation();
 	const navigate = useNavigate();
+	const [search, setSearch] = useState("");
 
 	const { data, isSuccess } = useGetAllQuery(0);
 
@@ -14,8 +19,14 @@ export default function HomePage() {
 	
 	return (
 		<main>
-			<div className="mt-20 ml-40">
-				{isSuccess && data.result.map(word => <WordItem key={word.id} word={word} />)}
+			<div className="mt-10 mx-40">
+				<TextInput value={search} onChange={value => setSearch(value)} className="mb-5" label={t("search")} />
+
+				{isSuccess && data.result.map(word => 
+					<div key={word.id} className="mb-3">
+						<WordItem word={word} />
+					</div>
+				)}
 			</div>
 
 			<div className="fixed bottom-5 right-5">
