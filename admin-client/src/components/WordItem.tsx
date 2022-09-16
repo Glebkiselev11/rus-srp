77e21c1f38c934d3c2d6d533fe3f2ba38f175f2c
Gleb Kiselev <12bny23gleb@gmail.com>
2022-09-16 21:18:09 +0200

    Frontend: Add border for word item

--- admin-client/src/components/WordItem.tsx
+++ admin-client/src/components/WordItem.tsx
@@ -7,6 +7,6 @@ interface WordItemProps {
 export function WordItem({ word }: WordItemProps) {
 
 	return (
-		<div>{word.rus} - {word.srp_cyrillic} - {word.srp_latin}</div>
+		<div className="border border-gray-200 py-2 px-4">{word.rus} - {word.srp_cyrillic} - {word.srp_latin}</div>
 	);
 }
