dfb52b8a233fc567d041d5c05a35f49087db1e01
Gleb Kiselev <12bny23gleb@gmail.com>
2023-05-27 18:12:45 +0200

    Update diesel for last version and add returning_clauses_for_sqlite_3_35; also add chrono; and fix all query db methods

--- server/src/db/user.rs
+++ server/src/db/user.rs
@@ -4,7 +4,7 @@ use diesel::SqliteConnection;
 type DbError = Box<dyn std::error::Error + Send + Sync>;
 use uuid::Uuid;
 
-pub fn add(new_user: NewUser, conn: &SqliteConnection) -> Result<User, DbError> {
+pub fn add(new_user: NewUser, conn: &mut SqliteConnection) -> Result<User, DbError> {
     use crate::db::schema::users::dsl::*;
 
     let user = User {
@@ -18,7 +18,7 @@ pub fn add(new_user: NewUser, conn: &SqliteConnection) -> Result<User, DbError> 
     Ok(user)
 }
 
-pub fn find(_username: &str, conn: &SqliteConnection) -> Result<Option<User>, DbError> {
+pub fn find(_username: &str, conn: &mut SqliteConnection) -> Result<Option<User>, DbError> {
     use crate::db::schema::users::dsl::*;
 
     let user = users
