e132aa643c1bde9c1edf9e326b5fe9ef4dc070e6
Gleb Kiselev <12bny23gleb@gmail.com>
2023-05-30 19:55:39 +0200

    Add some accessibililty for navbutton

--- admin-client/src/components/SideMenuNavButton.tsx
+++ admin-client/src/components/SideMenuNavButton.tsx
@@ -4,15 +4,24 @@ import { useLocation } from "react-router-dom";
 interface SideMenuNavButtonProps {
   link: string,
   label: string,
-  className: string
+  className?: string
 }
 
-export function SideMenuNavButton({ link, label, className }: SideMenuNavButtonProps) {
+export function SideMenuNavButton({ link, label, className = "" }: SideMenuNavButtonProps) {
 	const { pathname } = useLocation();
-	const  enabledClass = pathname === link ? "bg-purple-200" : "" ;
+	const  isActive = pathname === link;
+	const enabledClass = isActive ? "bg-purple-200" : "" ;
+
+	const combineClasses = (...classes: string[]): string => {
+		return classes.filter(Boolean).join(" ");
+	};
 
 	return (
-		<Link to={link} className={`text-sm py-3.5 px-4 rounded-xl hover:bg-purple-300 ${enabledClass} ${className}`}>
+		<Link 
+			to={link} 
+			className={combineClasses("text-sm py-3.5 px-4 rounded-xl hover:bg-purple-300", enabledClass, className)}
+			aria-current={isActive ? "page" : undefined}
+		>
 			{label}
 		</Link>
 	);
