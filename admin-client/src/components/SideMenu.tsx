60ceae021d3874ebd42b7e04cee3c39b8e5dd2cc
Gleb Kiselev <12bny23gleb@gmail.com>
2023-05-30 19:50:09 +0200

    Add some optimizations for SideMenu; Add enabled state for naviation button

--- admin-client/src/components/SideMenu.tsx
+++ admin-client/src/components/SideMenu.tsx
@@ -1,15 +1,21 @@
 import { useTranslation } from "react-i18next";
 import { SideMenuNavButton } from "./SideMenuNavButton";
+import { useMemo } from "react";
 
 export function SideMenu() {
 	const { t } = useTranslation();
 
+	const navigation = useMemo(() => [
+		{ link: "/", label: t("all-words"), className: "mb-1" },
+		{ link: "/settings", label: t("settings"), className: "mb-1" }
+	], [t]);
+
 	return (
 		<aside className="p-[12px] bg-white w-[265px] h-[100vh]">
 			<nav className="flex flex-col">
-				<SideMenuNavButton link="/" label={t("all-words")} />
-				<SideMenuNavButton link="/settings" label={t("settings")} />
-
+				{navigation.map(({ link, label, className }) => 
+					<SideMenuNavButton key={link} link={link} label={label} className={className}/>
+				)}
 			</nav>
 		</aside>
 	);
--- admin-client/src/components/SideMenuNavButton.tsx
+++ admin-client/src/components/SideMenuNavButton.tsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 interface SideMenuNavButtonProps {
   link: string,
-  label: string
+  label: string,
+  className: string
 }
 
-export function SideMenuNavButton({ link, label }: SideMenuNavButtonProps) {
+export function SideMenuNavButton({ link, label, className }: SideMenuNavButtonProps) {
+	const { pathname } = useLocation();
+	const  enabledClass = pathname === link ? "bg-purple-200" : "" ;
+
 	return (
-		<Link to={link} className="text-sm py-3.5 px-4 rounded-xl hover:bg-purple-300">
+		<Link to={link} className={`text-sm py-3.5 px-4 rounded-xl hover:bg-purple-300 ${enabledClass} ${className}`}>
 			{label}
 		</Link>
 	);
