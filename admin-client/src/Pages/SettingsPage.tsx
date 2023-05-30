87025cf8b17a61f30cf1176619cbc4412d91f376
Gleb Kiselev <12bny23gleb@gmail.com>
2023-05-30 23:12:59 +0200

    Add simple header component

--- admin-client/src/Pages/SettingsPage.tsx
+++ admin-client/src/Pages/SettingsPage.tsx
@@ -1,11 +1,19 @@
+import { useTranslation } from "react-i18next";
+import AppHeader from "../components/AppHeader";
 import { LanguageChanger } from "../components/LanguageChanger";
 
 export default function SettingsPage() {
+	const { t } = useTranslation();
+
 	return (
-		<main>
-			<div className="mt-10 mx-40">
-				<LanguageChanger/>
-			</div>
-		</main>
+		<>
+			<AppHeader title={t("settings")}></AppHeader>
+
+			<main>
+				<div className="mt-10 mx-40">
+					<LanguageChanger/>
+				</div>
+			</main>
+		</>
 	);
 }
---
+++ admin-client/src/components/AppHeader.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+
+interface AppHeaderProps {
+  children?: ReactNode,
+  title: string,
+}
+
+export default function AppHeader({ children, title }: AppHeaderProps) {
+	return (
+		<header className="bg-white py-4 px-6">
+			<h1 className="text-2xl bg-neutral-950">{title}</h1>
+
+			<div>
+				{children}
+			</div>
+		</header>
+	);
+}
