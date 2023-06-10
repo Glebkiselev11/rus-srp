3ea145da995227200f5f57005ab297e129108e00
Gleb Kiselev <12bny23gleb@gmail.com>
2023-06-10 22:54:18 +0200

    Add translations from serbian latin to serbian cyrillic

--- server/src/utils.rs
+++ server/src/utils.rs
@@ -1,5 +1,7 @@
 extern crate bcrypt;
 use bcrypt::{hash_with_salt, DEFAULT_COST};
+use std::collections::HashMap;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn hash_password(password: &str) -> Result<String, bcrypt::BcryptError> {
     let chars_salt: Vec<char> = std::env::var("SALT")
@@ -16,4 +18,85 @@ pub fn hash_password(password: &str) -> Result<String, bcrypt::BcryptError> {
     let hashed_password = hash_with_salt(password, DEFAULT_COST, salt)?;
 
     Ok(hashed_password.to_string())
+}
+
+pub fn serbian_latin_to_cyrillic(latin: &str) -> String {
+    let lat_to_cyr: HashMap<&str, &str> = [
+        ("A", "А"),
+        ("B", "Б"),
+        ("V", "В"),
+        ("G", "Г"),
+        ("D", "Д"),
+        ("Đ", "Ђ"),
+        ("E", "Е"),
+        ("Ž", "Ж"),
+        ("Z", "З"),
+        ("I", "И"),
+        ("J", "Ј"),
+        ("K", "К"),
+        ("L", "Л"),
+        ("Lj", "Љ"),
+        ("M", "М"),
+        ("N", "Н"),
+        ("Nj", "Њ"),
+        ("O", "О"),
+        ("P", "П"),
+        ("R", "Р"),
+        ("S", "С"),
+        ("T", "Т"),
+        ("Ć", "Ћ"),
+        ("U", "У"),
+        ("F", "Ф"),
+        ("H", "Х"),
+        ("C", "Ц"),
+        ("Č", "Ч"),
+        ("Dž", "Џ"),
+        ("Š", "Ш"),
+        ("a", "а"),
+        ("b", "б"),
+        ("v", "в"),
+        ("g", "г"),
+        ("d", "д"),
+        ("đ", "ђ"),
+        ("e", "е"),
+        ("ž", "ж"),
+        ("z", "з"),
+        ("i", "и"),
+        ("j", "ј"),
+        ("k", "к"),
+        ("l", "л"),
+        ("lj", "љ"),
+        ("m", "м"),
+        ("n", "н"),
+        ("nj", "њ"),
+        ("o", "о"),
+        ("p", "п"),
+        ("r", "р"),
+        ("s", "с"),
+        ("t", "т"),
+        ("ć", "ћ"),
+        ("u", "у"),
+        ("f", "ф"),
+        ("h", "х"),
+        ("c", "ц"),
+        ("č", "ч"),
+        ("dž", "џ"),
+        ("š", "ш"),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    let graphemes = UnicodeSegmentation::graphemes(latin, true).collect::<Vec<&str>>();
+
+    let mut cyrillic = String::new();
+    for grapheme in graphemes {
+        println!("{grapheme}");
+        match lat_to_cyr.get(grapheme) {
+            Some(cyr) => cyrillic.push_str(cyr),
+            None => cyrillic.push_str(grapheme),
+        }
+    }
+
+    cyrillic
 }
