e801486855e04296ae05950c77fea88e058f8206
Gleb Kiselev <12bny23gleb@gmail.com>
2023-06-24 22:37:42 +0200

    Add crop image function

--- admin-client/src/components/ImageSlider.tsx
+++ admin-client/src/components/ImageSlider.tsx
@@ -5,13 +5,22 @@ interface ImageSliderProps {
 }
 
 export function ImageSlider({images}: ImageSliderProps) {
+
+  const addCropImagaeParamsToUrl = (original: string): string => {
+    const url = new URL(original);
+    url.searchParams.append("fit", "crop");
+    url.searchParams.append("h", "300");
+    url.searchParams.append("w", "300");
+    return url.toString();
+  };
+
   return (
     <section className="overflow-x-scroll flex">
       {images.map(({id, src}) =>
         <img
           key={id}
           className="h-[150px] w-[150px] object-cover mr-2 shrink-0"
-          src={src.portrait}
+          src={addCropImagaeParamsToUrl(src.original)}
         />
       )}
     </section>
