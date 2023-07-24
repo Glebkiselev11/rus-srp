5c11a4b896e57ddbf2c561509e03147245859290
Gleb Kiselev <12bny23gleb@gmail.com>
2023-07-24 18:33:47 +0200

    Remove about page

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -10,19 +10,8 @@ const router = createRouter({
 			component: HomeView,
 		},
 		{
-			path: "/about",
-			name: "about",
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
-			component: () => import("../views/AboutView.vue"),
-		}, 
-		{
 			path: "/settings",
 			name: "settings",
-			// route level code-splitting
-			// this generates a separate chunk (About.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: () => import("../views/SettingsView.vue"),
 		},
 	],
