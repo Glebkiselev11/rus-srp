e500b5bf690c8b448890976b891b45406dde787b
Gleb Kiselev <12bny23gleb@gmail.com>
2023-07-27 11:48:55 +0200

    Add blank side menu component

---
+++ dashboard/src/components/AppSideMenu.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+
+export default defineComponent({
+	name: "AppSideMenu",
+	props: {
+		items: {
+			type: Array as PropType<string[]>,
+			required: true,
+		},
+	},
+	data() {
+		return {};
+	},
+});
+</script>
+
+
+<template>
+	<aside class="app-side-menu">
+		side menu
+		<nav>
+			<div
+				v-for="i in items"
+				:key="i"
+			>
+				{{ i }}	
+			</div>
+		</nav>
+	</aside>
+</template>
+
+
+<style scoped lang="scss">
+
+.app-side-menu {
+  width: 256px;
+  height: 100%;
+}
+
+</style>
--- dashboard/src/views/Panel/index.vue
+++ dashboard/src/views/Panel/index.vue
@@ -1,15 +1,31 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppSideMenu from "@/components/AppSideMenu.vue";
 import { RouterView } from "vue-router";
 
 export default defineComponent({
 	name: "PanelView",
 	components: {
 		RouterView,
+		AppSideMenu,
 	},
 });
 </script>
 
 <template>
-	<RouterView />
+	<div class="panel-view">
+		<AppSideMenu :items="['first', 'second']" />
+		<RouterView />
+	</div>
 </template>
+
+<style scoped lang="scss">
+.panel-view {
+	display: flex;
+	width: 100%;
+	height: 100vh;
+	background: yellow;
+}
+
+</style>
+
