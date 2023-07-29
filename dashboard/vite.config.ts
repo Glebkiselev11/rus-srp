6810334ac6f016d375f203eb0eaac8a6df0dde6d
Gleb Kiselev <12bny23gleb@gmail.com>
2023-07-29 11:43:30 +0200

    Fix typescript (imports, declaration) warnings

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from "node:url";
+import path from "path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
@@ -10,7 +10,7 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
+			"@": path.resolve(__dirname, "src"),
 			"vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
 		},
 	},
