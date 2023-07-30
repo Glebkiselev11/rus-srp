ac70bdebc186967382f07491c40567a6ca0a8bc1
Gleb Kiselev <12bny23gleb@gmail.com>
2023-07-30 17:30:31 +0200

    Fix return type from axios

--- dashboard/src/api/words.ts
+++ dashboard/src/api/words.ts
@@ -1,23 +1,23 @@
 import type { ListResponse, OptionalRequestParams } from "@/types/api";
 import type { DraftWord, Word } from "@/types/words";
-import axios from "axios";
+import axios, { type AxiosResponse } from "axios";
 
 export const WordsApi = {
 	ENDPOINT: "/api/v1/words",
 
-	query(params: OptionalRequestParams): Promise<ListResponse<Word>> {
+	query(params: OptionalRequestParams): Promise<AxiosResponse<ListResponse<Word>>> {
 		return axios.get(this.ENDPOINT, { params }); 
 	},
-	getById(id: string): Promise<Word> {
+	getById(id: string): Promise<AxiosResponse<Word>> {
 		return axios.get(`${this.ENDPOINT}/${id}`);
 	},
-	create(data: DraftWord): Promise<Word> {
+	create(data: DraftWord): Promise<AxiosResponse<Word>> {
 		return axios.post(this.ENDPOINT, data);
 	},
-	update(id: string, data: Word): Promise<Word> {
+	update(id: string, data: Word): Promise<AxiosResponse<Word>> {
 		return axios.put(`${this.ENDPOINT}/${id}`, data);
 	},
-	delete(id: string): Promise<void> {
+	delete(id: string): Promise<AxiosResponse<void>> {
 		return axios.delete(`${this.ENDPOINT}/${id}`);
 	},
 };
