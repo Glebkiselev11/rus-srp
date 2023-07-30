5bcfa0f7e76331646d48d5876456ae8ae127f156
Gleb Kiselev <12bny23gleb@gmail.com>
2023-07-30 17:48:25 +0200

    Add store for words

---
+++ dashboard/src/stores/words.ts
@@ -0,0 +1,23 @@
+import type { RequestParams } from "@/types/api";
+import { defineStore } from "pinia";
+import { WordsApi } from "@/api";
+import type { Word } from "@/types/words";
+
+export const useWordsStore = defineStore("words", {
+	state: () => ({
+		words: [] as Array<Word>,
+		count: 0,
+	}),
+	actions: {
+		async fetchWords(params: RequestParams) {
+			try {
+				const { data } = await WordsApi.query(params);
+				this.words = data.result;
+				this.count = data.count;
+			} catch (error) {
+				console.error(error);
+				alert(error);
+			}
+		},
+	},
+});
