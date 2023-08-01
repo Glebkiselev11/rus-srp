babfb52c51d372b8a42e4ff677bcf8da7263663b
Gleb Kiselev <12bny23gleb@gmail.com>
2023-08-01 19:28:07 +0200

    Add PageSection; Header components

--- dashboard/src/views/Panel/SettingsView.vue
+++ dashboard/src/views/Panel/SettingsView.vue
@@ -1,15 +1,29 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppTopBar from "@/components/AppTopBar.vue";
+import AppPageSection from "@/components/AppPageSection.vue";
 
 export default defineComponent({
 	name: "SettingsView",
+	components: {
+		AppTopBar,
+		AppPageSection,
+	},
 });
 
 </script>
 
 <template>
 	<main class="settings-view">
-		settings page
+		<AppTopBar>
+			<template #left>
+				<h3>{{ $t("settings") }}</h3>
+			</template>
+		</AppTopBar>
+
+		<div class="settings-view--content">
+			<AppPageSection :title="$t('general')" />
+		</div>
 	</main>
 </template>
 
@@ -17,9 +31,15 @@ export default defineComponent({
 <style scoped lang="scss">
 @import "@/styles/colors.scss";
 .settings-view {
-	background: $color-background;
 	width: 100%;
 	height: 100%;
+
+	&--content {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		padding-block-start: 16px;
+	}
 }
 
 </style>
