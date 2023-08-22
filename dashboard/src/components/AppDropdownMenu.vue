73ae74dd048ce0b83c6c61f4813c7829a3ecf848
Gleb Kiselev <12bny23gleb@gmail.com>
2023-08-22 19:48:59 +0200

    Close menu on clicking on menu item

--- dashboard/src/components/AppDropdownMenu.vue
+++ dashboard/src/components/AppDropdownMenu.vue
@@ -63,6 +63,10 @@ export default defineComponent({
 		setBottomMenuAngleVisibility(state: boolean) {
 			this.isBottomMenuVisible = state;
 		},
+		handdleClickOnItem(handler: () => void) {
+			handler();
+			this.closeMenu();
+		},
 	},
 });
 </script>
