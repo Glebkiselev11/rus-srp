557ca4a2b8e025425d0f3bc93d7d872d1c2d396e
Gleb Kiselev <12bny23gleb@gmail.com>
2023-09-12 19:24:30 +0200

    Add close button and subtitle for header

--- dashboard/src/components/AppHeader.vue
+++ dashboard/src/components/AppHeader.vue
@@ -1,12 +1,30 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppButton from "./AppButton.vue";
 
 export default defineComponent({
 	name: "AppHeader",
+	components: {
+		AppButton,
+	},
 	props: {
 		title: {
 			type: String,
 			default: null,
+		},
+		subtitle: {
+			type: String,
+			default: null,
+		},
+		closeButton: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	emits: ["close"],
+	methods: {
+		handleClose(): void {
+			this.$emit("close");
 		},
 	},
 });
@@ -14,11 +32,28 @@ export default defineComponent({
 
 <template>
 	<header class="app-header">
-		<h3>{{ title }}</h3>
+		<div>
+			<h3>{{ title }}</h3>
+			<span
+				v-if="subtitle"
+				class="app-header--subtitle"
+				v-text="subtitle"
+			/>
+		</div>
+
+		<AppButton
+			v-if="closeButton"
+			icon="close"
+			type="inline"
+			color="neutral"
+			class="app-header--close-button"
+			@click="handleClose"
+		/>
 	</header>
 </template>
 
 <style lang="scss" scoped>
+@import "@/styles/main.scss";
 
 .app-header {
   width: 100%;
@@ -27,6 +62,15 @@ export default defineComponent({
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+	&--close-button {
+		margin-inline-start: 16px;
+	}
+
+	&--subtitle {
+		@extend .text-body-2;
+		color: $color-text-secondary;
+	}
 }
 
 </style>
