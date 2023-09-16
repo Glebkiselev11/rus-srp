1277fae55d8d6dcfc7be2bbbc181afcf9c42ae7d
Gleb Kiselev <12bny23gleb@gmail.com>
2023-09-16 19:12:23 +0200

    Add modal wrap component

---
+++ dashboard/src/components/AppModal.vue
@@ -0,0 +1,78 @@
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+import AppHeader from "@/components/AppHeader.vue";
+
+export default defineComponent({
+	name: "AppModal",
+	components: {
+		AppHeader,
+	},
+	props: {
+		title: {
+			type: String,
+			required: true,
+		},
+		subtitle: {
+			type: String,
+			default: null,
+		},
+		visible: {
+			type: Boolean,
+			required: true,
+		},
+		size: {
+			type: String as PropType<"big" | "regular" | "compact">,
+			default: "regular",
+		},
+	},
+	emits: ["update:visible"],
+	methods: {
+		handleClose(): void {
+			this.$emit("update:visible", false);
+		},
+	},
+});
+
+</script>
+
+<template>
+	<div
+		v-show="visible"
+		class="app-modal-screen"
+	>
+		<div class="app-modal">
+			<AppHeader
+				:title="title"
+				:subtitle="subtitle"
+				close-button
+				@close="handleClose"
+			/>
+			<slot />
+		</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+@import "@/styles/main.scss";
+
+.app-modal-screen {
+	position: fixed;
+	z-index: 2;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+
+.app-modal {
+	border-radius: 16px;
+	padding: 8px;
+	background: $color-background-content-primary;
+}
+
+</style>
