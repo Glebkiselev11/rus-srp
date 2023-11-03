a19cdb2bcb4d2fa546571cd64dbcb38a29702b5d
Gleb Kiselev <12bny23gleb@gmail.com>
2023-11-03 14:53:02 +0100

    Clone category into draftCategory (if category exists) and make manipulation with draftCategory only; Add image and first (calculated) input for category

--- dashboard/src/components/AppCategoryForm.vue
+++ dashboard/src/components/AppCategoryForm.vue
@@ -1,13 +1,55 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppImagePreview from "./AppImagePreview.vue";
+import { useCategoriesStore } from "@/stores/categories";
+import { mapState } from "pinia";
+import type { Category, DraftCategory } from "@/types/categories";
+import { LanguageList, type LanguageCode } from "@/i18n";
+import AppInput from "./AppInput.vue";
 
 export default defineComponent({
 	name: "AppCategoryForm",
-	components: {},
+	components: { AppImagePreview, AppInput },
 	props: {
 		categoryId: {
 			type: Number,
 			default: undefined,
+		},
+	},
+	data() {
+		return {
+			draftCategory: {
+				rus: "",
+				eng: "",
+				srp_latin: "",
+				srp_cyrillic: "",
+				image: null,
+			} as DraftCategory,
+		};
+	},
+	computed: {
+		...mapState(useCategoriesStore, ["getCategoryById"]),
+		category(): Category | undefined {
+			return this.categoryId ? this.getCategoryById(this.categoryId) : undefined;
+		},
+		selectedLanguage(): LanguageCode {
+			return this.$i18n.locale as LanguageCode;
+		},
+		categoryName(): string {
+			return this.draftCategory[this.selectedLanguage];
+		},	
+		defaultImageSearchQuery(): string {
+			return this.draftCategory.eng;
+		},
+	},
+	created() {
+		if (this.category) {
+			this.draftCategory = { ...this.category };
+		}
+	},
+	methods: {
+		getLabelByKey(key: LanguageCode): string {
+			return LanguageList.find(({ value }) => value === key)?.label || "Not found label";
 		},
 	},
 
@@ -16,13 +58,40 @@ export default defineComponent({
 
 <template>
 	<div class="app-category-form">
-		hi from form
+		<div class="app-category-form__preview">
+			<AppImagePreview
+				size="96px"
+				:src="draftCategory.image"
+				:image-search-modal-subtitle="categoryName"
+				:default-image-search-query="defaultImageSearchQuery"
+				@update:src="draftCategory.image = $event"
+			/>
+
+			<AppInput
+				v-model="draftCategory[selectedLanguage]"
+				:label="getLabelByKey(selectedLanguage)"
+				width="400px"
+			/>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
+@import "@/styles/main.scss";
 
 .app-category-form {
-	padding: 16px;
+	padding-inline: 16px;
+	width: 598px;
+
+	&__preview {
+		display: flex;
+		column-gap: 24px;
+		width: 100%;
+		height: 144px;
+		padding: 24px;
+		background: $color-background;
+		border-radius: 16px;
+
+	}
 }
 </style>
