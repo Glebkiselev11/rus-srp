1b4e09daa7154bfc9e95ac59fd27f21b92cb6a04
Gleb Kiselev <12bny23gleb@gmail.com>
2023-11-05 12:14:34 +0100

    Rule for pluralization of translation

--- dashboard/src/i18n.ts
+++ dashboard/src/i18n.ts
@@ -14,8 +14,30 @@ export const LanguageList = [
 	{ value: "srp_cyrillic", label: "Српски" },
 ] as { value: LanguageCode; label: string }[];
 
+function slavicPluralizationRule(choice: number, choicesLength: number) {
+	if (choice === 0) {
+		return 0;
+	}
+
+	const teen = choice > 10 && choice < 20;
+	const endsWithOne = choice % 10 === 1;
+	if (!teen && endsWithOne) {
+		return 1;
+	}
+	if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+		return 2;
+	}
+
+	return choicesLength < 4 ? 2 : 3;
+}
+
 export default createI18n({
 	locale: localStorage.getItem(STORAGE_KEY) || "eng",
+	pluralizationRules: {
+		rus: slavicPluralizationRule,
+		srp_latin: slavicPluralizationRule,
+		srp_cyrillic: slavicPluralizationRule,
+	},
 	messages: {
 		eng,
 		rus,
--- dashboard/src/shims-vue.d.ts
+++ dashboard/src/shims-vue.d.ts
@@ -3,6 +3,7 @@ import { VueI18n } from "vue-i18n";
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $t: VueI18n["t"];
+    $tc: VueI18n["tc"];
     $i18n: VueI18n;
   }
 }
