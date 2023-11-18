25c9389bdec1f0515961941f11a45e234c850df3
Gleb Kiselev <12bny23gleb@gmail.com>
2023-11-18 16:47:53 +0100

    Add word form inputs

--- dashboard/src/components/WordFormComp.vue
+++ dashboard/src/components/WordFormComp.vue
@@ -1,11 +1,14 @@
 <script lang="ts">
 import { defineComponent, type PropType } from "vue";
-import ImageSectionComp from "./ImageSectionComp.vue";
 import type { DraftWord, Word } from "@/types/words";
+import { getLanguageName } from "@/common/translations";
+import ImageSectionComp from "./ImageSectionComp.vue";
+import ButtonComp from "./ButtonComp.vue";
+import InputComp from "./InputComp.vue";
 
 export default defineComponent({
 	name: "WordFormComp",
-	components: { ImageSectionComp },
+	components: { ImageSectionComp, ButtonComp, InputComp },
 	props: {
 		word: {
 			type: Object as PropType<Word>,
@@ -32,11 +35,20 @@ export default defineComponent({
 				return this.$t("new-word");
 			}
 		},
+		showFillAutoButton(): boolean {
+			return false;
+		},
 	},
 	created() {
 		if (this.word) {
 			this.draftWord = { ...this.word };
 		}
+	},
+	methods: {
+		getLanguageName,
+		autoFill() {
+			console.log("autoFill");
+		},
 	},
 });
 
@@ -54,6 +66,55 @@ export default defineComponent({
 				<span class="text-body-2">{{ subtitle }}</span>
 			</div>
 		</ImageSectionComp>
+
+		<div class="word-form__row">
+			<h3>{{ $t('translation') }}</h3>
+
+			<ButtonComp
+				v-show="showFillAutoButton"
+				icon="edit_note"
+				appearance="inline"
+				size="regular"
+				:label="$t('fill-in-auto')"
+				@click="autoFill"
+			/>
+		</div>
+
+		<InputComp
+			v-model="draftWord.rus"
+			appearance="outline"
+			disable-error-label
+			clear-button
+			:label="getLanguageName('rus')"
+			class="word-form__translation-input"
+		/>
+
+		<InputComp
+			v-model="draftWord.eng"
+			appearance="outline"
+			disable-error-label
+			clear-button
+			:label="getLanguageName('eng')"
+			class="word-form__translation-input"
+		/>
+
+		<InputComp
+			v-model="draftWord.srp_latin"
+			appearance="outline"
+			disable-error-label
+			clear-button
+			:label="getLanguageName('srp_latin')"
+			class="word-form__translation-input"
+		/>
+
+		<InputComp
+			v-model="draftWord.srp_cyrillic"
+			appearance="outline"
+			disable-error-label
+			clear-button
+			:label="getLanguageName('srp_cyrillic')"
+			class="word-form__translation-input"
+		/>
 	</div>
 </template>
 
@@ -64,6 +125,17 @@ export default defineComponent({
 	padding-inline: 16px;
 	padding-block-end: 20px;
 	width: 598px;
+
+	&__row {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-block: 20px;
+	}
+
+	&__translation-input {
+		margin-block: 20px;
+	}
 }
 
 </style>
