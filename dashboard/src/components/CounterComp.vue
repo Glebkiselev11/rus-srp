432d65fe82c5af57dd1b7bafe4cf2ef5339c0070
Gleb Kiselev <12bny23gleb@gmail.com>
2024-01-27 13:15:18 +0100

    Fix css tabs

--- dashboard/src/components/CounterComp.vue
+++ dashboard/src/components/CounterComp.vue
@@ -22,15 +22,15 @@ export default defineComponent({
 @import "@/styles/main.scss";
 
 .counter {
-  @include text-caption-2;
-  padding-block: 2px;
-  padding-inline: 6px;
+	@include text-caption-2;
+	padding-block: 2px;
+	padding-inline: 6px;
 	text-align: center;
-  height: 20px;
+	height: 20px;
 	min-width: 20px;
-  border-radius: 100px;
-  background: $color-background-content-primary;
-  color: $color-text-accent-primary;
+	border-radius: 100px;
+	background: $color-background-content-primary;
+	color: $color-text-accent-primary;
 }
 
 </style>
--- dashboard/src/components/SwitchComp.vue
+++ dashboard/src/components/SwitchComp.vue
@@ -47,76 +47,76 @@ export default defineComponent({
 @import "@/styles/main.scss";
 
 .switch {
-  position: relative;
-  width: 36px;
-  height: 20px;
+	position: relative;
+	width: 36px;
+	height: 20px;
 
-  input[type="checkbox"] {
-    opacity: 0;
-    width: 0;
-    height: 0;
+	input[type="checkbox"] {
+		opacity: 0;
+		width: 0;
+		height: 0;
 
-    &:focus-visible ~.switch__slider {
-      @include focus-visible;
-    }
-  }
+		&:focus-visible ~.switch__slider {
+			@include focus-visible;
+		}
+	}
 
-  &__slider {
-    position: absolute;
-    cursor: pointer;
-    width: inherit;
-    height: inherit;
-    border-radius: 50px;
-    transition: background-color .2s;
-    background: $color-background-content-secondary;
+	&__slider {
+		position: absolute;
+		cursor: pointer;
+		width: inherit;
+		height: inherit;
+		border-radius: 50px;
+		transition: background-color .2s;
+		background: $color-background-content-secondary;
 
-    &:before {
-      position: absolute;
-      content: "";
-      height: 16px;
-      width: 16px;
-      border-radius: 50%;
-      left: 2px;
-      bottom: 2px;
-      transition: .2s;
-      background-color: $color-icon-contrast;
-      filter: 
-        drop-shadow(0px 0px 4px rgba(2, 18, 38, 0.08)) 
-        drop-shadow(0px 4px 8px rgba(2, 18, 38, 0.04));
-    }
+		&:before {
+			position: absolute;
+			content: "";
+			height: 16px;
+			width: 16px;
+			border-radius: 50%;
+			left: 2px;
+			bottom: 2px;
+			transition: .2s;
+			background-color: $color-icon-contrast;
+			filter: 
+				drop-shadow(0px 0px 4px rgba(2, 18, 38, 0.08)) 
+				drop-shadow(0px 4px 8px rgba(2, 18, 38, 0.04));
+		}
 
-    &:hover {
-      background-color: $color-background-secondary-hovered;
-    }
-  }
+		&:hover {
+			background-color: $color-background-secondary-hovered;
+		}
+	}
 
-  &--checked {
-    .switch__slider {
-      background-color: $color-background-accent-primary;
-    }
+	&--checked {
+		.switch__slider {
+			background-color: $color-background-accent-primary;
+		}
 
-    .switch__slider:before {
-      transform: translateX(16px);
-    }
+		.switch__slider:before {
+			transform: translateX(16px);
+		}
 
-    &:hover {
-      .switch__slider {
-        background-color: $color-background-accent-primary-hovered;
-      }
-    }
-  }
+		&:hover {
+			.switch__slider {
+				background-color: $color-background-accent-primary-hovered;
+			}
+		}
+	}
 
-  &--disabled {
-    pointer-events: none;
+	&--disabled {
+		pointer-events: none;
 
-    .switch__slider {
-      background-color: $color-background-disabled;
-    }
+		.switch__slider {
+			background-color: $color-background-disabled;
+		}
 
-    .switch__slider:before {
-      background-color: $color-field-background;
-    }
-  }
+		.switch__slider:before {
+			background-color: $color-field-background;
+		}
+	}
 }
 
 </style>
