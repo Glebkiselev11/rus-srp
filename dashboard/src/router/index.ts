ac4ed8afb44f5be350049307e2de06e291f1abbd
Gleb Kiselev <12bny23gleb@gmail.com>
2024-01-28 13:35:41 +0100

    Rename history admin with dashboard

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-	history: createWebHistory("/admin"),
+	history: createWebHistory("/dashboard"),
 	routes: [
 		{
 			path: "/",
