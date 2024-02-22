99f738d72a0718ea77d3fc1d221e2dd155f30bfd
Gleb Kiselev <12bny23gleb@gmail.com>
2024-02-22 18:59:22 +0100

    Https for dashboard

--- dashboard/src/api/index.ts
+++ dashboard/src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://0.0.0.0:8080/";
+axios.defaults.baseURL = "https://0.0.0.0:8080/";
 
 export * from "./words";
 export * from "./images";
