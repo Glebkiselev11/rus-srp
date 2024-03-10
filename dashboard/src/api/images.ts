58e02cc5d971cc2fc207375d70c22da296821656
Gleb Kiselev <12bny23gleb@gmail.com>
2024-03-10 19:11:46 +0100

    Fix endoint urls

--- dashboard/src/api/images.ts
+++ dashboard/src/api/images.ts
@@ -3,7 +3,7 @@ import type { Image } from "@/types/images";
 import axios, { type AxiosResponse } from "axios";
 
 export const ImagesApi = {
-	ENDPOINT: "api/v1/private/services/images",
+	ENDPOINT: "/api/v1/private/services/images",
 
 	query(params: OptionalRequestParams): Promise<AxiosResponse<ListResponse<Image>>> {
 		return axios.get(`${this.ENDPOINT}/query`, { params }); 
--- dashboard/src/api/translations.ts
+++ dashboard/src/api/translations.ts
@@ -2,7 +2,7 @@ import type { TranslationObject } from "@/types/translations";
 import axios, { type AxiosResponse } from "axios";
 
 export const TranslationsApi = {
-	ENDPOINT: "api/v1/private/services/translation",
+	ENDPOINT: "/api/v1/private/services/translation",
 
 	translate(params: TranslationObject): Promise<AxiosResponse<TranslationObject>> {
 		return axios.post(`${this.ENDPOINT}/translate`, params);	
