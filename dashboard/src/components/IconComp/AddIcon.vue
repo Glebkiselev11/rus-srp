2de22e16fe213a12081635229911a2e6dc881f95
Gleb Kiselev <12bny23gleb@gmail.com>
2024-03-24 15:44:39 +0100

    fix: add icon size

--- dashboard/src/components/IconComp/AddIcon.vue
+++ dashboard/src/components/IconComp/AddIcon.vue
@@ -13,9 +13,14 @@ export default defineComponent({
 </script>
 
 <template>
-  <svg :width="size" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    :width="size"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path
-      d="M16.6667 10.8334H10.8333V16.6667H9.16666V10.8334H3.33333V9.16671H9.16666V3.33337H10.8333V9.16671H16.6667V10.8334Z"
+      d="M11.25 19V12.75H5V11.25H11.25V5H12.75V11.25H19V12.75H12.75V19H11.25Z"
     />
   </svg>
 </template>
