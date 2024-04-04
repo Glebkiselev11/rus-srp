a0583d854536c40cfc5e1df7946f22bba7f3e98f
Gleb Kiselev <12bny23gleb@gmail.com>
2024-04-04 18:18:02 +0200

    refactor: rename dashboard with admin/frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory("/dashboard"),
+  history: createWebHistory("/admin"),
   routes: [
     {
       path: "/",
