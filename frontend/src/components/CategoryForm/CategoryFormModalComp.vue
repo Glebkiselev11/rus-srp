d330c7ba557efb9a2cfc54f1a1a219f15f532e99
Gleb Kiselev <12bny23gleb@gmail.com>
2024-04-14 15:16:44 +0200

    refactor: rewrite category form modal comp into setup style

--- frontend/src/components/CategoryForm/CategoryFormModalComp.vue
+++ frontend/src/components/CategoryForm/CategoryFormModalComp.vue
@@ -1,83 +1,73 @@
-<script lang="ts">
-import { defineComponent, type PropType } from "vue";
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { useCategoriesActions } from "@/stores/categories/actions";
+import { capitalizeFirstLetter } from "@/common/utils";
+import type { LanguageCode } from "@/types/translations";
+import type { Id } from "@/types/api";
 import ModalComp from "@/components/ModalComp.vue";
 import CategoryFormComp from "./CategoryFormComp.vue";
-import { useCategoriesActions } from "@/stores/categories/actions";
-import { mapState } from "pinia";
-import type { LanguageCode } from "@/types/translations";
-import type { Category } from "@/types/categories";
 import ImagePreviewComp from "@/components/ImagePreviewComp.vue";
 import FormCloseConfirmationModalComp from "@/components/FormCloseConfirmationModalComp.vue";
-import { capitalizeFirstLetter } from "@/common/utils";
-import type { Id } from "@/types/api";
 
-export default defineComponent({
-  name: "CategoryFormModalComp",
-  components: {
-    ModalComp,
-    CategoryFormComp,
-    ImagePreviewComp,
-    FormCloseConfirmationModalComp,
-  },
-  props: {
-    // If provided category id, then form will be in edit mode
-    categoryId: {
-      type: Number as PropType<Id>,
-      default: undefined,
-    },
-  },
-  emits: ["close", "created"],
-  data() {
-    return {
-      // Mark as changed to prevent closing modal without confirmation
-      isChanged: false,
-      showCloseConfirmationModal: false,
-    };
-  },
-  computed: {
-    ...mapState(useCategoriesActions, ["getCategoryById"]),
-    category(): Category | undefined {
-      return this.categoryId
-        ? this.getCategoryById(this.categoryId)
-        : undefined;
-    },
-    title(): string {
-      return this.categoryId
-        ? this.$t("editing-category")
-        : this.$t("creation-category");
-    },
-    subtitle(): string {
-      return capitalizeFirstLetter(
-        this.category?.[this.$i18n.locale as LanguageCode] || ""
-      );
-    },
-    closeConfirmationTitle() {
-      return Boolean(this.category)
-        ? this.$t("modal-exit-confirmation.edit-category-title")
-        : this.$t("modal-exit-confirmation.creation-category-title");
-    },
-    closeConfirmationCancelButtonLabel() {
-      return Boolean(this.category)
-        ? this.$t("modal-exit-confirmation.continue-editing")
-        : this.$t("modal-exit-confirmation.continue-creation");
-    },
-  },
-  methods: {
-    tryClose() {
-      if (this.isChanged) {
-        this.showCloseConfirmationModal = true;
-      } else {
-        this.close();
-      }
-    },
-    close() {
-      this.$emit("close");
-    },
-    setChanged(status: boolean) {
-      this.isChanged = status;
-    },
-  },
+const { t, locale } = useI18n();
+const categoryActions = useCategoriesActions();
+
+const props = defineProps<{
+  categoryId?: Id; // If provided category id, then form will be in edit mode
+}>();
+
+const emit = defineEmits<{
+  (e: "close"): void;
+  (e: "created", id: Id): void;
+}>();
+
+const isChanged = ref(false);
+const showCloseConfirmationModal = ref(false);
+
+const category = computed(() => {
+  return props.categoryId
+    ? categoryActions.getCategoryById(props.categoryId)
+    : undefined;
 });
+
+const title = computed(() => {
+  return props.categoryId ? t("editing-category") : t("creation-category");
+});
+
+const subtitle = computed(() => {
+  return capitalizeFirstLetter(
+    category.value?.[locale.value as LanguageCode] || ""
+  );
+});
+
+const closeConfirmationTitle = computed(() => {
+  return Boolean(category.value)
+    ? t("modal-exit-confirmation.edit-category-title")
+    : t("modal-exit-confirmation.creation-category-title");
+});
+
+const closeConfirmationCancelButtonLabel = computed(() => {
+  return Boolean(category.value)
+    ? t("modal-exit-confirmation.continue-editing")
+    : t("modal-exit-confirmation.continue-creation");
+});
+
+function tryClose() {
+  if (isChanged.value) {
+    showCloseConfirmationModal.value = true;
+  } else {
+    close();
+  }
+}
+
+function setChanged(status: boolean) {
+  isChanged.value = status;
+}
+
+function close() {
+  emit("close");
+}
 </script>
 
 <template>
