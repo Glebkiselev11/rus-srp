c38c08edd2ba8b5a055484a9062df57a0e8cde92
Gleb Kiselev <12bny23gleb@gmail.com>
2024-04-17 22:50:21 +0200

    refactor: rewrite images into vue-query

--- frontend/src/api/services/images.ts
+++ frontend/src/api/services/images.ts
@@ -5,9 +5,18 @@ import { ApiTransport } from "@/api/apiTransport";
 export const ImagesService = {
   ENDPOINT: "/api/v1/private/services/images",
 
-  query(params: OptionalRequestParams) {
-    return ApiTransport.query<ListResponse<Image>>(`${this.ENDPOINT}/query`, {
-      params,
-    });
+  async query(params: OptionalRequestParams) {
+    const { data } = await ApiTransport.query<ListResponse<Image>>(
+      `${this.ENDPOINT}/query`,
+      {
+        params,
+      }
+    );
+
+    return {
+      images: data.result,
+      count: data.count,
+      offset: data.offset,
+    };
   },
 };
