55ee489f3af213195c97828f8abe75dd2be27e29
Gleb Kiselev <12bny23gleb@gmail.com>
2024-09-26 19:58:46 +0200

    feat: convert serbians on server

--- server/src/handlers/services/translation.rs
+++ server/src/handlers/services/translation.rs
@@ -3,6 +3,7 @@ use reqwest::{
     self,
     header::{HeaderMap, HeaderValue, CONTENT_TYPE},
 };
+use serbian_cyrillic_latin_conversion::Convertion;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
@@ -34,29 +35,47 @@ struct ResponseData {
 const URL: &str = "https://api.openai.com/v1/chat/completions";
 
 pub async fn translate(body: web::Json<AiTranslatePayload>) -> actix_web::Result<impl Responder> {
-    let client = reqwest::Client::new();
+    let srp_latin = if !body.srp_cyrillic.is_empty() && body.srp_latin.is_empty() {
+        Convertion::from_cyrillic(&body.srp_cyrillic)
+    } else if !body.srp_latin.is_empty() {
+        body.srp_latin.clone()
+    } else {
+        String::new()
+    };
 
-    let body = json!({
+    let body_to_send = json!({
         "model": "gpt-4",
         // response_format is needed for 4o model
         // "response_format": { "type": "json_object" },
         "messages": [{
             "role": "system",
             "content": format!(
-                "eng: '{}', rus: '{}', srp_cyrillic: '{}', srp_latin: '{}' return
+                "eng: '{}', rus: '{}', srp_latin: '{}' return
                 json object with filled translation for empty strings",
-                body.eng, body.rus, body.srp_cyrillic, body.srp_latin
+                body.eng, body.rus, srp_latin
             ),
         }]
     });
 
-    let res = client.post(URL).headers(headers()).json(&body).send().await;
+    let res = reqwest::Client::new()
+        .post(URL)
+        .headers(headers())
+        .json(&body_to_send)
+        .send()
+        .await;
 
     match res {
         Ok(resp) => {
             let data: ResponseData = resp.json().await.unwrap();
             let content = &data.choices[0].message.content;
-            let translation_result = serde_json::from_str::<serde_json::Value>(&content).unwrap();
+            let mut translation_result =
+                serde_json::from_str::<serde_json::Value>(&content).unwrap();
+
+            translation_result["srp_cyrillic"] = if !body.srp_cyrillic.is_empty() {
+                body.srp_cyrillic.as_str().into()
+            } else {
+                Convertion::from_latin(&translation_result["srp_latin"].as_str().unwrap()).into()
+            };
 
             Ok(HttpResponse::Ok().json(translation_result))
         }
